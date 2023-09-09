9440ad09d8eebe00309d346ef94fb6c902d8dff5
sarvensis <ivank_ri@mail.ru>
2023-09-09 16:02:41 +0600

    Fix domain

--- frontend/src/providers/env.ts
+++ frontend/src/providers/env.ts
@@ -12,8 +12,7 @@ const readApiBaseFromEnv = (): string => {
   // Priority is given to same host in the browser when environemnt is production
   if (
     import.meta.env.PROD &&
-    !document.location.host.startsWith("localhost") &&
-    !isIpAddressAndNotDomain(document.location.host)
+    !document.location.host.startsWith("localhost")
   ) {
     return `https://${document.location.host}`;
   } else if (import.meta.env.BASE_URL && import.meta.env.BASE_URL !== "/") {
